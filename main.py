e82fdf0c27c523423feb75ee166f8f0881d6b12f
benedekposfay <benedek@posfay.com>
2022-05-26 20:33:41 +0200

    init

---
+++ main.py
@@ -0,0 +1,28 @@
+import discord
+import logging
+
+# Bot Access Token -----------------------------------------------------------------------------------------------------
+f = open("token.txt", "r")
+token = f.read()
+# Bot Access Token -----------------------------------------------------------------------------------------------------
+
+# Logger setup ---------------------------------------------------------------------------------------------------------
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+# Logger setup ---------------------------------------------------------------------------------------------------------
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+
+client = MyClient()
+client.run(token)
+
